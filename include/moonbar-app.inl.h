ee01ad2c99a61f0cad79473d9155014259dcb1eb
tazz <tasman.vose@gmail.com>
2026-01-14 17:24:08 -0500

    refactor: more code cleanup

--- include/moonbar-app.inl.h
+++ include/moonbar-app.inl.h
@@ -10,6 +10,7 @@ typedef struct _BarApp {
   GtkApplication* app;
   GtkWidget* window;
   GtkCenterBox* box;
+  //TODO(@s0cks): convert to GtkBox
   GtkWidget* left;
   GtkWidget* center;
   GtkWidget* right;
@@ -26,5 +27,39 @@ void mbar_app_free(BarApp* app);
 bool mbar_app_run(BarApp* app);
 void mbar_app_load_style(BarApp* app, const char* filename);
 char* mbar_app_get_cwd(BarApp* app);
-
 char* mbar_get_home_from_env();
+
+#define DEFINE_BOX_GET(Side)            \
+  static inline GtkBox*                 \
+  mbar_get_##Side(BarApp* app) {        \
+    ASSERT(app);                        \
+    return GTK_BOX(app->Side);          \
+  }
+DEFINE_BOX_GET(left);
+DEFINE_BOX_GET(center);
+DEFINE_BOX_GET(right);
+#undef DEFINE_BOX_GET
+
+#define DEFINE_BOX_APPEND(Side)                             \
+  static inline void                                        \
+  mbar_append_##Side(BarApp* app, GtkWidget* widget) {      \
+    ASSERT(app);                                            \
+    ASSERT(widget);                                         \
+    gtk_box_append(mbar_get_##Side(app), widget);           \
+  }
+DEFINE_BOX_APPEND(left);
+DEFINE_BOX_APPEND(center);
+DEFINE_BOX_APPEND(right);
+#undef DEFINE_BOX_APPEND
+
+#define DEFINE_BOX_PREPEND(Side)                            \
+  static inline void                                        \
+  mbar_prepend_##Side(BarApp* app, GtkWidget* widget) {     \
+    ASSERT(app);                                            \
+    ASSERT(widget);                                         \
+    gtk_box_prepend(mbar_get_##Side(app), widget);          \
+  }
+DEFINE_BOX_PREPEND(left);
+DEFINE_BOX_PREPEND(center);
+DEFINE_BOX_PREPEND(right);
+#undef DEFINE_BOX_PREPEND
--- src/app.h
+++ src/app.h
@@ -7,7 +7,6 @@
 void mbar_error(BarApp* app, const char* error);
 void mbar_on_tick(BarApp* app);
 void mbar_publish(BarApp* app, const char* event);
-void mbar_add_left(BarApp* app, GtkWidget* widget);
 void mbar_exec_cb(BarApp* app, Callback* cb);
 
 #endif // MBAR_APP_H
